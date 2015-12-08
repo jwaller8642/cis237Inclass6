af94cb4a78883abef600c305279cac862e0426ea
Jason Waller <jwaller8642@mail.kvcc.edu>
2015-12-08 15:51:56 -0500

    Adds tons

--- cis237InClass6/App_Start/RouteConfig.cs
+++ cis237InClass6/App_Start/RouteConfig.cs
@@ -12,12 +12,19 @@ namespace cis237InClass6
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            /*
+            routes.MapRoute( 
+                name: "Mine",
+                url: "my/own/route",
+                defaults: new {controller = "Cars", action = "Index"}
+                );
+             */
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            
         }
     }
 }
--- cis237InClass6/Controllers/HomeController.cs
+++ cis237InClass6/Controllers/HomeController.cs
@@ -16,7 +16,8 @@ namespace cis237InClass6.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+            // Added this call "SomeThingElse: to the view bag
+            ViewBag.SomeThingElse = "SomeThing Else";
             return View();
         }
 
@@ -24,6 +25,13 @@ namespace cis237InClass6.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+        //Added new method. it will get called by the default route, if i point to /Home/Foo
+        public ActionResult Foo()
+        {
+            //Returns some text
+            //return Content("This is my page");
             return View();
         }
     }
